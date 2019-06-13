9c581f7af3a3d1c28c9b15aed4b409868622d3c2
Sani Kamal <sanikamal223@gmail.com>
2019-06-13 13:25:48 +0530

    create hello.php

---
+++ learning-php/hello.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Hello World</title>
+</head>
+    <body>
+        <p>Hello from html</p>
+        
+        <?php
+            echo "<p>Hello from PHP</p>";
+        ?>
+        
+        
+    </body>
+
+</html>
